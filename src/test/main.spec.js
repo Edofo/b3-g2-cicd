7b3fb6cbf85b228e960d3e4be733a544502c565c
Edofo <48387251+Edofo@users.noreply.github.com>
2026-01-30 10:22:23 +0100

    test: fixed ci

--- src/test/main.spec.js
+++ src/test/main.spec.js
@@ -2,7 +2,7 @@ const { describe, it, expect } = require("@jest/globals");
 
 describe("GET /", () => {
   it("should return 200", () => {
-    const response = "Hello Woorld";
+    const response = "Hello World";
     expect(response).toBe("Hello World");
   });
 });
