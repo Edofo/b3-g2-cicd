8e11c69f6e679259f65dda45d22c76ab38ffd00d
Edofo <48387251+Edofo@users.noreply.github.com>
2026-01-30 10:19:45 +0100

    test: failed ci

--- src/test/main.spec.js
+++ src/test/main.spec.js
@@ -2,7 +2,7 @@ const { describe, it, expect } = require("@jest/globals");
 
 describe("GET /", () => {
   it("should return 200", () => {
-    const response = "Hello World";
+    const response = "Hello Woorld";
     expect(response).toBe("Hello World");
   });
 });
